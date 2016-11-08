39803ce6636b132079615a86dac566f2f72b6f52
csawala <chris.sawala@gmail.com>
2016-11-08 02:31:58 +0000

    download image by hardcode

--- download_avatars.js
+++ download_avatars.js
@@ -30,3 +30,16 @@ getRepoContributors("jquery", "jquery", function(result) {
     console.log("Result:", user['avatar_url'])
   })
 })
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+       .on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+         console.log('Response Status Code: ', response.statusCode);
+       })
+       .pipe(fs.createWriteStream(filePath));
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
