9aa097cd571f31001ed795a04d5870f081274cc6
csawala <chris.sawala@gmail.com>
2016-11-08 02:24:59 +0000

    functioning avatar_url grab

--- download_avatars.js
+++ download_avatars.js
@@ -2,21 +2,31 @@
 
 require('dotenv').config()
 const request = require('request')
+const fs = require('fs');
 const gitUser = process.env.GIT_USER
 const gitToken = process.env.TOKEN
 
 
 console.log("Welcome to the Github Avatar Downloader!")
-console.log(gitUser, gitToken)
 
 function getRepoContributors(repoOwner, repoName, cb){
   const requestURL = 'https://'+ gitUser + ':' + gitToken
     + '@api.github.com/repos/' + repoOwner + '/'
     + repoName + '/contributors';
-  console.log(requestURL);
+
+  request.get({
+    url: requestURL,
+    headers: {
+      'User-Agent': 'Ron Burgundy'
+    }
+  }, function(err, response, body){
+      const data = JSON.parse(body)
+      cb(data)
+  })
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err)
-  console.log("Result:", result)
+getRepoContributors("jquery", "jquery", function(result) {
+  result.forEach(function(user){
+    console.log("Result:", user['avatar_url'])
+  })
 })
