e2ab16e6e63ce6fd24151c10b862b298b95d08a1
csawala <chris.sawala@gmail.com>
2016-11-08 01:33:33 +0000

    add base file calling user/token

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,22 @@
 'use strict';
 
+require('dotenv').config()
 const request = require('request')
+const gitUser = process.env.GIT_USER
+const gitToken = process.env.TOKEN
+
 
 console.log("Welcome to the Github Avatar Downloader!")
+console.log(gitUser, gitToken)
 
+function getRepoContributors(repoOwner, repoName, cb){
+  const requestURL = 'https://'+ gitUser + ':' + gitToken
+    + '@api.github.com/repos/' + repoOwner + '/'
+    + repoName + '/contributors';
+  console.log(requestURL);
+}
 
-// getRepoContributors
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err)
+  console.log("Result:", result)
+})
