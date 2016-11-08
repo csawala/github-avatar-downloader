469636ceb777b74435755ab70539866565fba674
csawala <chris.sawala@gmail.com>
2016-11-08 19:52:12 +0000

    reupload w/ suggestions from evaluation

--- download_avatars.js
+++ download_avatars.js
@@ -7,13 +7,12 @@ const fs = require('fs');
 // pull from hidden .env to prevent uploading user & token
 const gitUser = process.env.GIT_USER
 const gitToken = process.env.TOKEN
+// acquire necessary user and repo info from CLI
 const input = process.argv.slice(2)
 
 
-
-console.log("====== Welcome to the Github Avatar Downloader! ======")
-
-function getRepoContributors(repoOwner, repoName, cb){
+// ============================ DECLARE FUNCTIONS ============================
+const getRepoContributors = function(repoOwner, repoName, cb){
   const requestURL = 'https://'+ gitUser + ':' + gitToken
     + '@api.github.com/repos/' + repoOwner + '/'
     + repoName + '/contributors';
@@ -22,7 +21,7 @@ function getRepoContributors(repoOwner, repoName, cb){
     url: requestURL,
     // site requires validation found here under headers
     headers: {
-      'User-Agent': 'Ron Burgundy'
+      'User-Agent': 'LHL Avatar Exercise'
     }
   }, function(err, response, body){
       const data = JSON.parse(body)
@@ -30,6 +29,19 @@ function getRepoContributors(repoOwner, repoName, cb){
   })
 }
 
+const downloadImageByURL = function(url, filePath) {
+  request.get(url)
+       .on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+         console.log('Downloaded: ', url, " to: ", filePath);
+       })
+       .pipe(fs.createWriteStream(filePath));
+}
+
+// ============================ BEGIN CODE ============================
+console.log("====== Welcome to the Github Avatar Downloader! ======")
 // only fires if argv input has 2 values, i.e. user && token
 if (input.length === 2){
   getRepoContributors(input[0], input[1], function(result) {
@@ -39,15 +51,4 @@ if (input.length === 2){
   })
 } else {
   console.log("Please try again with the Owner and Repo information")
-}
-
-function downloadImageByURL(url, filePath) {
-  request.get(url)
-       .on('error', function (err) {
-         throw err;
-       })
-       .on('response', function (response) {
-         console.log('Downloaded: ', url, " to: ", filePath);
-       })
-       .pipe(fs.createWriteStream(filePath));
 }
